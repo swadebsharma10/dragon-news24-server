2a9bf8f6da716c5b6cfd44c166ce9070c33ca801
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-03 13:33:34 +0600

    make daynamic routing api allnews, and category base news

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
   res.send('Dragon News is Running')
@@ -14,6 +16,29 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req,res)=>{
     res.send(categories)
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    if(id === '08'){
+      res.send(news)
+    }
+    else{
+      const categoryNews = news.filter(news => news.category_id === id);
+      res.send(categoryNews)
+    }
+  
+})
+
+// news related api
+app.get('/news', (req,res)=>{
+  res.send(news)
+})
+
+app.get('/news/:id', (req,res)=>{
+  const id = req.params.id;
+  const selectedNews = news.find(news => news._id === id);
+  res.send(selectedNews)
 })
 
 app.listen(port, () => {
